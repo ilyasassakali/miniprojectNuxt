6e0ea2bfdab4715968c6e48fb3803ab18bcb1aeb
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2023-09-26 10:33:31 +0200

    initialisedProject

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora soluta quis animi sapiente cupiditate ratione odio aspernatur nostrum? Debitis, odit.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
