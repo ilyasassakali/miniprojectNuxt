2fdf2e9b32a0a2e3972455b2819f764c22d7e6a7
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2023-09-27 09:52:42 +0200

    fetchData

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         <header class="shadow-sm bg-white">
             <nav class="container mx-auto p-4 flex justify-between">
-                 <NuxtLink  class="font-bold">Home</NuxtLink> 
+                <img class="logo" src="../images/cart.png" alt="">
                 <ul class="flex gap-4">
                     <li> <NuxtLink to="/">Home</NuxtLink> </li>
                     <li> <NuxtLink to="/about">About</NuxtLink> </li>
@@ -22,6 +22,10 @@
 <style scoped>
 .router-link-exact-active{
     color: rgb(129, 255, 140);
+}
+
+.logo{
+    width: 50px;
 }
 
 </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-        <h1>products</h1>
-    </div>
-</template>
-
-<script>
-    definePageMeta({
-        layout: 'products'
-    })
-</script>
-
-<style lang="scss" scoped>
-
-</style>  
+        <h1 class="text-3xl font-bold mb-4">Products</h1>
+    <Suspense>
+      <template #default>
+        <div class="grid grid-cols-4 gap-4 p-4">
+          <div v-for="p in products" :key="p.id" class="bg-white p-4 shadow rounded">
+            <NuxtLink :to="`/products/${p.id}`">
+              <img :src="p.image" :alt="p.title" class="w-full h-auto mb-2" />
+              <div class="text-center">{{ p.title }}</div>
+            </NuxtLink>
+          </div>
+        </div>
+      </template>
+  
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </Suspense>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      const { data: products } = useFetch('https://fakestoreapi.com/products');
+      return { products };
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  /* Vos styles ici */
+  </style>
+  
